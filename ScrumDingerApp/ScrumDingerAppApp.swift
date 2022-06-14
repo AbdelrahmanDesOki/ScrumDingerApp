c7a2de64d69491f1e7c4175d7e11a4afd9711474
abdelrahmandesoki <desoki.abdelrahman@gmail.com>
2022-06-14 15:20:21 +0200

    Initial Commit

---
+++ ScrumDingerApp/ScrumDingerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerAppApp.swift
+//  ScrumDingerApp
+//
+//  Created by Abd Elrahman Desoki on 14/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
